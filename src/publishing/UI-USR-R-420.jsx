55c117d8cb9bd89a4e84d987d0414e57e2b74be7
강덕원 <dw.kang@s-on.kr>
2026-01-22 18:09:17 +0900

    docs: 퍼블리싱 마이비지니스  서브 페이지 작업

--- src/publishing/UI-USR-R-420.jsx
+++ src/publishing/UI-USR-R-420.jsx
@@ -1,14 +1,10 @@
-import React from "react";
-import { useState } from 'react';
+ import React from "react";
 
 import SideNavigation from "../components/ui/SideNavigation";
 import Breadcrumb from "../components/ui/Breadcrumb";
-import Popup from "../components/ui/Popup";
 
 
-const UI_USR_R_451 = () => {
-  // 팝업 동작
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
+const UI_USR_R_420 = () => {
 
   const navigationData = {
     depth1Title: "신청·발급",
@@ -60,7 +56,7 @@ const UI_USR_R_451 = () => {
       <div className="contents">
         <Breadcrumb items={breadcrumbItems} />
         <div className="page-title-wrap" data-type="responsive">
-          <h2 className="h-tit">기업 기본정보</h2>
+          <h2 className="h-tit">비밀번호 수정</h2>
         </div>
         
         <div className="conts-wrap form-confirm">
@@ -79,47 +75,68 @@ const UI_USR_R_451 = () => {
           <p className="txt-caution">※ 안전한 중소벤처24 서비스 이용을 위해 키보드보안 프로그램 적용을 권장합니다.</p>
 
            <dl className="on-form-row large">
-            <div className="form-row-item ">
+            <div className="form-row-item">
               <dt className="form-row-label">
                 <label htmlFor="input_01">
-                  아이디
+                  현재 비밀번호
                 </label>
               </dt>
               <dd className="form-row-content">
                 <div className="form-wrapper w-220">
-                 <input type="text" id="input_01" className="krds-input small" value="smes2025" disabled></input>
+                 <input type="password" id="input_01" className="krds-input small" placeholder="비밀번호를 입력해주세요." ></input>
                 </div>
               </dd>
             </div>
             <div className="form-row-item">
-              <dt className="form-row-label">
+              <dt className="form-row-label flex-start">
                 <label htmlFor="input_02">
-                  비밀번호
+                  새 비밀번호
                 </label>
               </dt>
               <dd className="form-row-content">
                 <div className="form-wrapper w-220">
                  <input type="password" id="input_02" className="krds-input small" placeholder="비밀번호를 입력해주세요." ></input>
                 </div>
+                 <p className="form-hint">
+                  비밀번호는 영문자(대·소문자), 숫자, 특수문자중 두가지를 조합하여 8자~20자 이내로 입력하세요. <br />
+                 ( 사용가능 특수문자 : !, @, #, $, %, ^, &, *, (, ), -, =, _, + )</p>
+              </dd>
+            </div>
+            <div className="form-row-item">
+              <dt className="form-row-label flex-start">
+                <label htmlFor="input_03">
+                  새 비밀번호 확인
+                </label>
+              </dt>
+              <dd className="form-row-content">
+                <div className="form-wrapper w-220">
+                 <input type="password" id="input_03" className="krds-input small" placeholder="비밀번호를 입력해주세요." ></input>
+                </div>
+                 <p className="form-hint point">새 비밀번호를 8자 이상 , 영문자, 숫자, 특수문자 조합을 입력해야 합니다.</p>
               </dd>
             </div>
           </dl>
         </div>
 
-
-         {/* bottom btn */}
-        <div className="onboard-btm-btngroup bt-0 btn-single">
-          <div> 
-            <button type="button" className="krds-btn primary xlarge">
-              다음 단계
-            </button>
+        <div className="conts-wrap mt-64 certify-conts">
+          <h3 className="sec-tit">기업 인증 </h3>
+          <div className="certify-cont-box">
+            <div className="certify-cont-item">
+              <div className="certify-cont-img"></div>
+              <button type="button" className="krds-btn medium primary">인증하기</button>
+            </div>
           </div>
+          <ul className="krds-info-list decimal" role="list">
+            <li role="listitem">
+              인증 관련 문의 <br />
+              - NICE평가정보(주) 고객센터 Tel : 1600-1522
+            </li>
+          </ul>
         </div>
-      </div> 
 
-     
+      </div> 
     </>
   );
 };
 
-export default UI_USR_R_451;
+export default UI_USR_R_420;
