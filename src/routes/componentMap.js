025909348cb92150c26faef2f553358a9fed1722
KSW <won4698@u-cube.kr>
2026-01-27 20:42:33 +0900

    정책금융 개발진행중

--- src/routes/componentMap.js
+++ src/routes/componentMap.js
@@ -83,6 +83,12 @@ export const componentMap = {
   'M_PIIO_00077': {
     component: UI_USR_L_030,
     layout: SubpageLayoutWithMenu,
+    children: [
+      {
+        path: ':plcyFnncNo',
+        component: UI_USR_R_031,
+      },
+    ],
   },
 
   // 증명서 발급
