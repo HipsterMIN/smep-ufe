4509cabd93e3ed0e4fbf6600c0c5a7f688301efd
박동욱 <>
2026-04-22 22:33:47 +0900

    fix : sso callback url `tipa` 삭제
    fix : sso callback url `tipa` 삭제

--- src/utils/keycloakGetAuthCode.js
+++ src/utils/keycloakGetAuthCode.js
@@ -4,7 +4,7 @@ const KEYCLOAK_URL = 'https://www.smes.go.kr/isso-dev/qsign';
 const KEYCLOAK_JOIN = 'https://www.smes.go.kr/onepass-dev/conversion/step1';
 const REALM = 'ucube-qsign';
 const CLIENT_ID = 'smes-tipa-01';
-const REDIRECT_URI = 'https://www.smes-tipa.go.kr/home-dev/sso'; // 우리 사이트 콜백 주소
+const REDIRECT_URI = 'https://www.smes.go.kr/home-dev/sso'; // 우리 사이트 콜백 주소
 
 // 로그인후 원패스 가입 유도시
 export function onePassJoin() {
