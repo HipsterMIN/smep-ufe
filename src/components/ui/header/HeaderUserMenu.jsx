4889b0e3cb597d55678733d91cd05d25833f4a9e
박동욱 <>
2026-05-04 15:34:22 +0900

    fix(header): 외부 사이트 임시 버튼 일부 제거

--- src/components/ui/header/HeaderUserMenu.jsx
+++ src/components/ui/header/HeaderUserMenu.jsx
@@ -17,26 +17,6 @@ export default function HeaderUserMenu({
   onMyPage,
   onSwitchContext,
 }) {
-  const handleCollaborationInfoSystemClick = () => {
-    //협업정보시스템
-    const url = 'https://www.smes.go.kr/isso-dev/qsign/realms/ucube-qsign/protocol/openid-connect/auth?response_type=code&client_id=cobiz&redirect_uri=https://www.smes.go.kr/cobiz-iam/iam/oauth/loginCallback.do&scope=openid';
-    if (!url) {
-      return;
-    }
-
-    window.open(url, '_blank', 'noopener,noreferrer');
-  };
-
-  const handleBusinessProjectGroupClick = () => {
-    //비지니스사업단
-    const url = 'https://www.smes.go.kr/isso-dev/qsign/realms/ucube-qsign/protocol/openid-connect/auth?response_type=code&client_id=bizlink&redirect_uri=https://www.smes.go.kr/bizlink-iam/iam/oauth/loginCallback.do&scope=openid';
-    if (!url) {
-      return;
-    }
-
-    window.open(url, '_blank', 'noopener,noreferrer');
-  };
-
   const handleMnaClick = () => {
     const url = 'https://www.smes.go.kr/isso-dev/qsign/realms/ucube-qsign/protocol/openid-connect/auth?response_type=code&client_id=mna&redirect_uri=https://www.smes.go.kr/mna-iam/iam/oauth/loginCallback.do&scope=openid';
     if (!url) {
@@ -68,20 +48,6 @@ export default function HeaderUserMenu({
 
   const externalLinkButtons = (
     <>
-      <button
-        type="button"
-        className="btn-navi none-icon on-mobile-none"
-        onClick={handleCollaborationInfoSystemClick}
-      >
-        협업정보시스템
-      </button>
-      <button
-        type="button"
-        className="btn-navi none-icon on-mobile-none"
-        onClick={handleBusinessProjectGroupClick}
-      >
-        비지니스사업단
-      </button>
       <button
         type="button"
         className="btn-navi none-icon on-mobile-none"
