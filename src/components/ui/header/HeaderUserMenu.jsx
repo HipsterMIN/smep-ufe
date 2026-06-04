a3bda242b62800a68f03a7725e3ba250abf31d4b
larble.work <larble.work@u-cube.kr>
2026-06-05 08:06:52 +0900

    fix: 기존 중소벤처24로 이동하는 링크 추가
    - "중소벤처24" 링크 두 곳에 추가: 메뉴 상단과 로그인 영역
    - 모바일에서 숨김 처리된 버튼에 "on-mobile-none" 클래스 적용

--- src/components/ui/header/HeaderUserMenu.jsx
+++ src/components/ui/header/HeaderUserMenu.jsx
@@ -107,7 +107,9 @@ export default function HeaderUserMenu({
       <>
         {/*externalLinkButtons*/}
 
-
+        <a className="btn-go on-mobile-none" href="https://smes.go.kr/main" target="_blank">
+          기존 중소벤처24 이동
+        </a>
         {isSsoLogin && (
           <button type="button" className="btn-navi onepass on-mobile-none" onClick={onOnePassConfig}>
               중기 통합회원 관리
@@ -160,7 +162,6 @@ export default function HeaderUserMenu({
             </button>
           ) : null}
         </div> */}
-
         <strong className="pc-only">{displayName}</strong>
 
         {showSessionTimer ? (
@@ -203,6 +204,10 @@ export default function HeaderUserMenu({
           중기 통합회원 로그인
         </button>
       )}
+
+      <a className="btn-go on-mobile-none" href="https://smes.go.kr/main" target="_blank">
+        기존 중소벤처24 이동
+      </a>
       <button type="button" className="btn-navi login on-mobile-none" onClick={onLogin}>
         로그인
       </button>
